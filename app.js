b7f027768f37377b4e779c8f622c67ab8645f79e
leishufeng <leishufeng@icibei.com>
2020-02-11 16:20:01 +0800

    初始化提交

---
+++ app.js
@@ -0,0 +1,20 @@
+new Vue({
+    el: '#vue-app',
+    data: {
+        health: 100,
+        ended: false
+    },
+    methods: {
+        punch() {
+            this.health -= 10;
+            console.log(this.heath)
+            if (this.health <= 0) {
+                this.ended = true;
+            }
+        },
+        restart() {
+            this.health = 100;
+            this.ended = false;
+        }
+    }
+})
